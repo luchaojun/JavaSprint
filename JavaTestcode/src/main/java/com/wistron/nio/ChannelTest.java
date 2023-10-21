f21c9559560a84f1b4323fd0ff84afb81ffc8378
Chaojun_Lu <Chaojun_Lu@wistron.com>
2023-10-21 16:04:11 +0800

    Add the Channel code test

--- JavaTestcode/src/main/java/com/wistron/nio/ChannelTest.java
+++ JavaTestcode/src/main/java/com/wistron/nio/ChannelTest.java
@@ -8,6 +8,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 public class ChannelTest {
     //测试写数据到文件
@@ -40,5 +41,144 @@ public class ChannelTest {
             System.out.println(byteBuffer.capacity());
         }
         fileChannel.close();
+    }
+
+    //对BufferByte中的put(BufferByte b)方法进行测试
+    @Test
+    public void channelTest2() throws IOException {
+        FileInputStream fos = new FileInputStream("niotest2");
+        FileChannel fileChannel = fos.getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4*4);
+        fileChannel.read(byteBuffer);
+        ByteBuffer byteBuffer2 = ByteBuffer.allocate(4*4*2);
+        System.out.println(byteBuffer2.capacity());
+        System.out.println(byteBuffer2.limit());
+        System.out.println(byteBuffer2.position());
+        byteBuffer.flip();
+        byteBuffer2.put(byteBuffer);
+        byteBuffer2.flip();
+        System.out.println(byteBuffer2.capacity());
+        System.out.println(byteBuffer2.limit());
+        System.out.println(byteBuffer2.position());
+        while(byteBuffer2.hasRemaining()){
+            System.out.println((char)byteBuffer2.get());
+        }
+    }
+
+
+    //创建数据给channelTest4方法进行测试
+    @Test
+    public void channelTest3() throws IOException {
+        FileOutputStream fos = new FileOutputStream("niotest2");
+        FileChannel fileChannel = fos.getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        for(int i=0;i<1000;i++){
+            byteBuffer.put(("qin"+"\n").getBytes());
+            byteBuffer.flip();
+            fileChannel.write(byteBuffer);
+            byteBuffer.clear();
+        }
+        fileChannel.close();
+    }
+
+    //测试channel进行数据的读取
+    @Test
+    public void channelTest4() throws IOException {
+        FileInputStream fis = new FileInputStream("niotest2");
+        FileChannel fileChannel = fis.getChannel();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024 * 1024);
+        ByteBuffer byteBuffer2 = ByteBuffer.allocate(1024);
+        int read = fileChannel.read(byteBuffer);
+        while (read != -1){
+            byteBuffer.flip();
+            while (byteBuffer.hasRemaining()){
+                byte b = byteBuffer.get();
+                if(b == 10){
+                    byteBuffer2.flip();
+                    String line = Charset.forName("utf-8").decode(byteBuffer2).toString();
+                    System.out.println("line===="+line);
+                    byteBuffer2.clear();
+                }else{
+                    if(byteBuffer2.hasRemaining()){
+                        byteBuffer2.put(b);
+                    }else{
+                        byteBuffer2 = reAllocate(byteBuffer2);
+                        byteBuffer2.put(b);
+                    }
+                }
+            }
+            byteBuffer.clear();
+            read = fileChannel.read(byteBuffer);
+        }
+        byteBuffer2.flip();
+        String line = Charset.forName("utf-8").decode(byteBuffer2).toString();
+        System.out.println(line);
+        fileChannel.close();
+    }
+
+    //channelTest方法byteBuffer2大小不够的时候， 进行扩容
+    private ByteBuffer reAllocate(ByteBuffer byteBuffer2) {
+        System.out.println("=================reAllocate操作============================");
+        byteBuffer2.flip();
+        ByteBuffer reAllocateByteBuffer = ByteBuffer.allocate(byteBuffer2.capacity() * 2);
+        reAllocateByteBuffer.put(byteBuffer2);
+        System.out.println(reAllocateByteBuffer.capacity());
+        System.out.println(reAllocateByteBuffer.limit());
+        System.out.println(reAllocateByteBuffer.position());
+        return reAllocateByteBuffer;
+    }
+
+    //复制一个图片
+    @Test
+    public void channelTest5() throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        FileInputStream fis = new FileInputStream("nv.jpg");
+        FileOutputStream fos = new FileOutputStream("nv-copy.jpg");
+        FileChannel fisChannel = fis.getChannel();
+        FileChannel fosChannel = fos.getChannel();
+        int picRead = fisChannel.read(byteBuffer);
+        while (picRead != -1){
+            byteBuffer.flip();
+            fosChannel.write(byteBuffer);
+            byteBuffer.clear();
+            picRead = fisChannel.read(byteBuffer);
+        }
+        fisChannel.close();
+        fosChannel.close();
+    }
+
+    //从目标通道中去复制原通道数据
+    @Test
+    public void channelTest6() throws IOException {
+        FileInputStream fis = new FileInputStream("nv.jpg");
+        FileOutputStream fos = new FileOutputStream("nv-copy.jpg");
+        FileChannel fisChannel = fis.getChannel();
+        FileChannel fosChannel = fos.getChannel();
+        System.out.println("fosChannel.position="+fosChannel.position()+",fosChannel.size()="+fosChannel.size());
+        System.out.println("fisChannel.position="+fisChannel.position()+",fisChannel.size()="+fisChannel.size());
+        fosChannel.transferFrom(fisChannel, fisChannel.position(), fisChannel.size());
+        System.out.println("After transferFrom, fosChannel.position="+fosChannel.position()+",fosChannel.size()="+fosChannel.size());
+        fisChannel.close();
+        fosChannel.close();
+    }
+
+    //把原通道数据复制到目标通道
+    @Test
+    public void channelTest7() throws IOException {
+        FileInputStream fis = new FileInputStream("nv.jpg");
+        FileOutputStream fos = new FileOutputStream("nv-copy.jpg");
+        FileChannel fisChannel = fis.getChannel();
+        FileChannel fosChannel = fos.getChannel();
+        System.out.println("fosChannel.position="+fosChannel.position()+",fosChannel.size()="+fosChannel.size());
+        System.out.println("fisChannel.position="+fisChannel.position()+",fisChannel.size()="+fisChannel.size());
+        fisChannel.transferTo(fisChannel.position(), fisChannel.size(), fosChannel);
+        System.out.println("After transferFrom, fosChannel.position="+fosChannel.position()+",fosChannel.size()="+fosChannel.size());
+    }
+
+    //分散读取聚集写入
+    @Test
+    public void channelTest8() throws IOException {
+
     }
 }
