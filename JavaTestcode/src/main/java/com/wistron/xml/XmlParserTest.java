f325883c04e55f18dc08f1c3116002c152ae7f70
Chaojun_Lu <Chaojun_Lu@wistron.com>
2023-11-06 00:37:49 +0800

    Add the cherry-pick and format-patch study

--- JavaTestcode/src/main/java/com/wistron/xml/XmlParserTest.java
+++ JavaTestcode/src/main/java/com/wistron/xml/XmlParserTest.java
@@ -3,13 +3,14 @@ package com.wistron.xml;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Stack;
@@ -29,11 +30,45 @@ public class XmlParserTest {
     }
 
     @Test
-    public void testSAXParser(){
+    public void testSAXParser() throws ParserConfigurationException, SAXException, IOException {
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        SAXParser saxParser = spf.newSAXParser();
+        File file = new File("for_xml_parser.xml");
+        saxParser.parse(file, new MySaxParserHandler());
 
+//        XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+//        xmlReader.setContentHandler(new MySaxParserHandler());
+//        xmlReader.parse(new InputSource("for_xml_parser.xml"));
     }
 }
 
 class MySaxParserHandler extends DefaultHandler{
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        System.out.println("========================startElement===========================");
+        System.out.println("uri="+uri);
+        System.out.println("localName="+localName);
+        System.out.println("qName="+qName);
+        System.out.println("===============================================================");
+    }
 
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        System.out.println("========================characters===========================");
+        System.out.println("str="+new String(ch, start, length));
+        System.out.println("start="+start);
+        System.out.println("length="+length);
+        System.out.println("===============================================================");
+
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        System.out.println("========================endElement===========================");
+        System.out.println("uri="+uri);
+        System.out.println("localName="+localName);
+        System.out.println("qName="+qName);
+        System.out.println("===============================================================");
+
+    }
 }
