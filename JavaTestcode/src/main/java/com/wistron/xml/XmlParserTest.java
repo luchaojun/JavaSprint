168cdfdeb4e931ae594f8e4250bff8c6ed308e43
Chaojun_Lu <Chaojun_Lu@wistron.com>
2023-11-10 22:48:02 +0800

    Add xml study

--- JavaTestcode/src/main/java/com/wistron/xml/XmlParserTest.java
+++ JavaTestcode/src/main/java/com/wistron/xml/XmlParserTest.java
@@ -69,6 +69,5 @@ class MySaxParserHandler extends DefaultHandler{
         System.out.println("localName="+localName);
         System.out.println("qName="+qName);
         System.out.println("===============================================================");
-
     }
 }
