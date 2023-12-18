acbc651d902045947f8c91e7462df4d47fb80000
Chaojun_Lu <Chaojun_Lu@wistron.com>
2023-12-19 00:12:40 +0800

    Add the jdt @Nullable, @NonNull Test Add the jetbrains @Nullable, @NotNull Test

--- JavaTestcode/src/main/java/com/wistron/annotation/NullAnnotationTest.java
+++ JavaTestcode/src/main/java/com/wistron/annotation/NullAnnotationTest.java
@@ -1,7 +1,8 @@
 package com.wistron.annotation;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+//import org.jetbrains.annotations.NotNull;
+//import org.jetbrains.annotations.Nullable;
+import org.eclipse.jdt.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,33 +13,39 @@ import java.util.List;
  * @Function： Null/SuppressWarning Annotation Test
  */
 public class NullAnnotationTest {
-    public static void testNullable(@Nullable String a) {
+//    public static void testJetBrainsNullable(@Nullable String a) {
+//        if (a == null) {
+//            System.out.println("a=" + a);
+//        }
+//    }
+//
+//    public static void testJetBrainsNonNull(@NotNull String a) {
+//        System.out.println("a=" + a);
+//    }
+
+    public static void testEclipseNullable(@Nullable String a) {
         if (a == null) {
             System.out.println("a=" + a);
         }
     }
 
-    public static void testNonNull(@NotNull String a) {
-        System.out.println("a=" + a);
-    }
-
-    public static void testEclipseNonNull(@NotNull String a) {
-        System.out.println("a=" + a);
+    public static String testEclipseNonNull(@NonNull String a) {   //@NonNul的作用是调用的方法如果参数是null, 会变黄
+        return a;
     }
 
     //        @SuppressWarnings({"unused"})   //消除变量没有被使用过的检查
     //        @SuppressWarnings("rawtypes") //消除多态的检查
-    @SuppressWarnings({"unused", "unchecked", "rawtypes", "MismatchedQueryAndUpdateOfCollection"})
+    @SuppressWarnings({"unused", "unchecked", "rawtypes","MismatchedQueryAndUpdateOfCollection"})
     public void testSuppressWarnings(){
         int i = 0;
-        List list = new ArrayList<>();
+        List list = new ArrayList();
         list.add("123");
     }
 
     public static void main(String[] args) {
-        testNullable(null);
-//        testNonNull(null);
-        testEclipseNonNull(null);
-
+//        testJetBrainsNullable(null);
+//        testJetBrainsNonNull(null);
+        testEclipseNullable(null);
+        testEclipseNonNull("a");
     }
 }
