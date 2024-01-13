18accfe391c5276c23dd63df5b082e8627966e6f
Chaojun_Lu <Chaojun_Lu@wistron.com>
2024-01-13 20:35:17 +0800

    Add mysql study

--- MySql/Mysql_SQL_Backup/mysql.sql
+++ MySql/Mysql_SQL_Backup/mysql.sql
@@ -1,5 +1,13 @@
-create procedure test2(IN a int, IN b int, OUT c int)
+drop procedure if exists test;
+
+create procedure test(in a int,inout x int)
 begin
-select * from tb_emp;
+	set x = a + x + 100;
 end;
+
+set @total = 3;
+
+call test(2, @total);
+
+select @total from dual;
 
