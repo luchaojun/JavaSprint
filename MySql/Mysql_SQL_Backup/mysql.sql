c1b8e4d96f7441bcb759fd19b54aa669e375d719
Chaojun_Lu <Chaojun_Lu@wistron.com>
2024-01-19 17:58:02 +0800

    Add cursor sql

--- MySql/Mysql_SQL_Backup/mysql.sql
+++ MySql/Mysql_SQL_Backup/mysql.sql
@@ -1,14 +1,25 @@
-drop function if exists test;
-
-create function test(emp_id int)
-returns varchar(100)
-reads sql data
+drop procedure if exists user_cursor;
+create procedure user_cursor(out res varchar(1000))
 begin
-    declare name varchar(20);
-    select username into name from tb_emp where id=emp_id;
-    return name;
+    declare emp_username varchar(20);
+    declare emp_name varchar(10);
+    declare emp_entrydate date;
+    declare i int default 0;
+    declare string varchar(100);
+    declare count int default 0;
+    declare MyCursor cursor for select username, name, entrydate from tb_emp;
+    select count(*) into count from tb_emp;
+    open MyCursor;
+    while i<count do
+    	fetch MyCursor into emp_username, emp_name, emp_entrydate;
+    	set i=i+1;
+    	select concat_ws('#', emp_username, emp_name, emp_entrydate) into string;
+    	set res=concat_ws(',', res, string);
+    end while;
+    close MyCursor;
 end;
 
-select test(1);
-select test(2);
-select test(3);
+set @temp="";
+call user_cursor(@temp);
+select @temp from dual;
+ 
